6183066c4431183d600c74fe8c0d8e36ea81a027
김희웅 <hank29206880@gmail.com>
2022-06-23 17:21:34 +0900

    make it better

--- server/index.js
+++ server/index.js
@@ -19,7 +19,18 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(err.message)
 })
 
-
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../public/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'public', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+  }
 
 app.use(errorHandler)
 
